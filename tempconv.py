571580398372904ae8d061e8eab107e0ee99f83c
DevXiTzCodez <sohailkhannn.0525@gmail.com>
2025-05-14 17:31:02 +0530

    Add files via upload

---
+++ tempconv.py
@@ -0,0 +1,16 @@
+while True:
+    unit = input("Enter temperature in Celsius - C / Fahrenheit - F or type q to quit: ")
+    if unit == "q":
+        break
+    temp = float(input("Enter the temperature: "))
+    
+    if unit == "C":
+        temp = round((9 * temp) / 5 + 32, 1)
+        print(f"The temperature in Fahrenheit is: {temp}°F")
+    elif unit == "F":
+        temp = round((temp - 32) * 5 / 9, 1)
+        print(f"The temperature in Celsius is: {temp}°C")
+    else:
+        print(f"{unit} is an invalid unit of measurement")
+ 
+print("Byee!")
